3397fb3570180923ca2a77806c9af808d1cabaf4
Emiliano Vázquez Banda <aoe2_emi@hotmail.com>
2020-11-25 23:00:46 -0600

    Añadir: Ejercicio Terminado

--- 1.js
+++ 1.js
@@ -1,29 +1,65 @@
+let Nombres;
+let Correos;
+
 /* Obtener elementos del localstorage */
-let Nombres = [localStorage.getItem("Nombres")];
-let Correos = [localStorage.getItem("Correos")];
+function ObtenerlocalStorage(){
+    /* Variables de intercambio temporales */
+    let lSNombres = localStorage.getItem("Nombres");
+    let lSCorreos = localStorage.getItem("Correos");
 
-function ObtenerTexto(){
-    const txtNombre = $("#nombre").val();
-    const txtCorreo = $("#email").val();
-
-    Nombres.push(txtNombre);
-    Correos.push(txtCorreo);
+    /* Sino hay elementos en el localStorage, crea un array */
+    Nombres = lSNombres ? JSON.parse(lSNombres) : [];
+    Correos = lSCorreos ? JSON.parse(lSCorreos) : [];
 }
 
+/* Guargar arrays en el localStorage */
 function GuardarlocalStorage(){
-    localStorage.setItem("Nombres", Nombres);
-    localStorage.setItem("Correos", Correos);
+    localStorage.setItem("Nombres", JSON.stringify(Nombres));
+    localStorage.setItem("Correos", JSON.stringify(Correos));
 }
+
+/* Guardar valor de inputs en los arrays */
+function ObtenerTexto(){
+    Nombres.push($("#nombre").val());
+    Correos.push($("#email").val());
+}
+
+/* Limpiar controles */
 function LimpiarControles(){
     $("#nombre").val("");
     $("#email").val("");
 }
 
+/* Evitar duplicado de los elementos de la tabla */
+function LimpiarTabla(){$("#salida tr td").remove();}
+
 $(document).ready(function(){
+    /* Boton guardar */
     $("#boton-guardar").click(function(){
+        ObtenerlocalStorage();
         ObtenerTexto();
         GuardarlocalStorage();
         LimpiarControles();
     });
 
+    /* Boton limpiar */
+    $("#boton-limpia").click(function(){LimpiarControles();});
+
+    /* Boton ver */
+    $("#boton-ver").click(function(){
+        ObtenerlocalStorage();
+        /* Limpiar tabla */
+        LimpiarTabla();
+
+        /* Mostrar contenido utilizando un solo forEach */
+        Nombres.forEach((Nombre, i) => {
+            let Correo = Correos[i];
+            $("#salida").append("<tr><td>"+Nombre+"</td><td>"+Correo+"</td></tr>");
+        });
+    });
+
+    $("#boton-lS").click(function(){
+        localStorage.clear();
+        LimpiarTabla();
+    })
 });
